51aad4e2bce7da8dc13fbf7bf5f99ad3b255e1ad
JahirPineda <138241041+JahirPineda@users.noreply.github.com>
2026-05-22 03:07:55 -0400

    Delete msvc-ordenes directory

--- msvc-ordenes/src/main/java/cl/duoc/msvc_ordenes/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package cl.duoc.msvc_ordenes.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String mensaje) {
-        super(mensaje);
-    }
-}
