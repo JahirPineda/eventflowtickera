344412c9f26027b3c2ef008246c6750b5dc24066
JahirPineda <138241041+JahirPineda@users.noreply.github.com>
2026-05-22 03:08:20 -0400

    Delete msvc-recintos directory

--- msvc-recintos/src/main/resources/db/migration/V1__init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS recintos (
-                                        id INT AUTO_INCREMENT PRIMARY KEY,
-                                        nombre VARCHAR(255) NOT NULL,
-    direccion VARCHAR(255) NOT NULL,
-    ciudad VARCHAR(255) NOT NULL,
-    capacidad INT NOT NULL,
-    imagen_url VARCHAR(255),
-    activo BOOLEAN NOT NULL DEFAULT TRUE
-    );
